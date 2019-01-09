84c57685c2330d7b7d144413345853e89d9b57cf
Jayce Azua <jayce.azua@gmail.com>
2019-01-09 02:26:51 -0500

    Added swift controllers to the views

---
+++ Collector/CreateCollectableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CreateCollectableViewController.swift
+//  Collector
+//
+//  Created by Jayce Azua on 1/9/19.
+//  Copyright © 2019 Jayce Azua. All rights reserved.
+//
+
+import UIKit
+
+class CreateCollectableViewController: UIViewController {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var titleTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func mediaFolderTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
